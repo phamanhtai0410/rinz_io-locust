027600868b0b66c4dd985a68ab1dc4dc3de0fcea
ttsang96 <taitatsang@gmail.com>
2022-06-04 10:24:58 +0700

    add marketplace api

--- api/market.py
+++ api/market.py
@@ -12,12 +12,12 @@ from pydash import get
 from helper.wallet import Wallet
 import json
 
-with open("fake_stag_accounts.json") as f:
-    accounts = json.load(f)
+# with open("fake_stag_accounts.json") as f:
+#     accounts = json.load(f)
 
 
 class MarketService(HttpUser):
-    wait_time = constant_throughput(1)
+    # wait_time = constant_throughput(1)
     #
     # def __init__(self, *args, **kwargs):
     #     super().__init__(*args, **kwargs)
@@ -59,6 +59,26 @@ class MarketService(HttpUser):
     @task
     def get_marketplace(self):
         obj = self.client.get(f'/v1/marketplace')
-        # print(obj.json())
 
+
+    @task
+    def get_marketplace_report(self):
+        obj = self.client.get(f'/v1/marketplace/report')
+
+        
+    @task
+    def get_marketplace_nft_detail(self):
+        obj = self.client.get(f'/v1/marketplace/report/nft', params={
+            'contract': "0xe6eac9440270750f1d444676335a94205e764969",
+            'token_id': 1
+        })    
+
+    
+    # @task
+    # def submit_marketplace_event(self):
+    #     obj = self.client.post(f'/v1/marketplace/event', json={
+    #         "txn_hash": "0xDA8b7341a9C8E2a7E4e5eC384Fe70b9F9bF8DF36",
+    #         "address": "0x8204f26687bed8a8c4c3dd8cb6fba5cb5160e34084130b6f2afe944f706b1830"
+    #     }, headers={'content-type': 'application/json'}) 
+      
 

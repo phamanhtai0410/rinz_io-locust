98452faab08e41e2baf4d74f8e04f05ccbbf4ff1
Pham Anh Tai <phamanhtai0410@gmail.com>
2022-06-07 11:31:20 +0700

    _stag_3_ add campaign test

--- api/campaign.py
+++ api/campaign.py
@@ -4,63 +4,78 @@
         -
         -
 """
+from locust import HttpUser, constant_throughput, task
+from pydash import get
+
+from helper.wallet import Wallet
 import json
-from datetime import datetime, timedelta
-from time import sleep
-import pydash
-from locust import HttpUser, task, between, constant_throughput
-from random import choice, random, uniform, choices, randint
 
-# Opening JSON file
-# Load account for test
-with open('/fake_accounts.json', "r") as f:
-    fake_accounts = json.load(f)
-
-
-def convert_datetime_to_string(date: datetime,
-                               _format="%Y%m%d%H%M%S") -> str:
-    return date.strftime(_format)
+with open("fake_stag_accounts.json") as f:
+    accounts = json.load(f)
 
 
 class TestCampaign(HttpUser):
     wait_time = constant_throughput(1)
 
     def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
-        self.account = None
-
-    def login(self, account):
-        obj = self.client.post('/v1/id/auth/admin/login', json=account)
-        if obj.status_code != 200:
-            print('Login', obj.text)
-            raise Exception
-        return pydash.get(obj.json(), 'data.access_token')
+        super().__init__(*args, **kwargs)
+        _account = accounts.pop()
+        print(_account)
+        self.wallet = Wallet(
+            public_address=get(_account, 'address'),
+            private_key=get(_account, 'private_key'),
+            host=''
+        )
 
     def on_start(self):
-        _account = fake_accounts.pop()
-        self.account = _account
-        _token = self.login(self.account)
+        """
+        - Login
+        :return:
+        """
+
+        obj = self.client.get('/v1/id/auth/message', params={
+            'public_address': self.wallet.public_address
+        })
+        _json = obj.json()
+
+        _msg = get(_json, 'data.sign_msg')
+        _signature = self.wallet.sign_msg(_msg)
+        _nonce = get(_json, 'data.nonce')
+        print(_signature)
+
+        _res = self.client.post("/v1/id/auth/login", json={
+            "public_address": self.wallet.public_address,
+            "signature": _signature,
+            "nonce": _nonce
+        }, headers={'content-type': 'application/json'})
+        _json = _res.json()
+        _access_token = get(_json, 'data.access_token', default=None)
         self.client.headers = {
-            'Authorization': f'Bearer {_token}'
+            'Authorization': f'Bearer {_access_token}'
         }
 
-    @task(1)
-    def get_list_campaigns(self):
-        pass
+    @task
+    def get_list_owned_campaigns(self):
+        obj = self.client.get(f'/v1/campaign/get_list')
 
+    @task
+    def get_details_of_campaign(self):
+        obj = self.client.get('/v1/campaign/details')
 
-    #
-    # @task(5)
-    # def get_batches(self):
-    #     obj = self.client.get('/v1/transaction/pos/batches')
-    #     if obj.status_code != 200:
-    #         print(obj.text)
-    #     else:
-    #         _data = obj.json()
-    #         _sections = pydash.get(_data, 'data.sections', default=[])
-    #         if _sections:
-    #             _param = choice(_sections)
-    #             _trans = self.client.get('/v1/transaction/pos/batch', params=_param)
-    #             if _trans.status_code != 200:
-    #                 print(_trans.text)
+    @task
+    def get_details_campaign_admin_site(self):
+        campaign_id = ""
+        obj = self.client.get(f'/v1/campaign/admin/details/{campaign_id}')
+
+    @task
+    def get_hot_campaigns(self):
+        obj = self.client.get("/v1/campaign/hot")
+
+    @task
+    def get_nft_type_details(self):
+        _payload = {
+            "contract_address": "",
+            "index_type": 1
+        }
+        obj = self.client.post("/v1/campaign/nft_details", _payload)
 
